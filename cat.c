6ed66bfff734eda1c07adb1ec8f84b726aef04a9
m3ndax <adrian.goessl@outlook.com>
2022-06-12 20:22:08 +0200

    update code and comment

--- cat.c
+++ cat.c
@@ -1,38 +1,35 @@
-#include <fcntl.h>
+/*include the libraries*/
 #include <stdio.h>
+#include <fcntl.h>
 #include <unistd.h>
 
-/*Using direct system calls instead of the standard library, 
- *this program should be somewhat faster.
+/*using direct system-calls instead of the standard library,
+ *this program should be somewhat faster
  */
- 
-#define BUFSIZE		4096
+
+ /*define the BUFSIZE*/
+ #define BUFSIZE 4096
 
 void filecopy(int ifd, int ofd);
 
+/*main part of the program*/
 int main(int argc, char *argv[])
 {
-	int		fd;
-	
+	int	fd;
+
 	if(argc == 1)
-		
 		filecopy(0, 1);
-		
 	else
-		
 		while(--argc > 0)
-			
 			if((fd = open(*++argv, O_RDONLY, 0)) == -1)
 			{
 				printf("Error: Cannot open %s!\n", *argv);
-				return 1;
 			}
 			else
 			{
 				filecopy(fd, 1);
 				close(fd);
 			}
-			
 	return 0;
 }
 
@@ -40,8 +37,7 @@ void filecopy(int ifd, int ofd)
 {
 	char	buf[BUFSIZE];
 	int		n;
-	
+
 	while((n = read(ifd, buf, BUFSIZE)) > 0)
-		
 		write(ofd, buf, n);
 }
