211c99e00a7287e66449f4a2ad83a0eccf78b70d
m3ndax <adrian.goessl@outlook.com>
2022-06-20 21:17:21 +0200

    update comment

--- if.c
+++ if.c
@@ -1,12 +1,14 @@
+/*include the libraries*/
 #include <stdio.h>
 
+/*define the main function*/
 int main()
 {
-    /*local varaiable definition*/
+    /*local variable definition*/
     int a = 100;
     int b = 200;
 
-    /*check the boolean conditon*/
+    /*check the boolean condition*/
     if(a == 100)
     {
         /*if condition is true then check the following*/
@@ -16,6 +18,8 @@ int main()
             printf("Value of a is 100 and b is 200\n");
         }
     }
+
+    /*print the output*/
     printf("Exact value of a is : %d\n", a);
     printf("Exact value of b is : %d\n", b);
 
