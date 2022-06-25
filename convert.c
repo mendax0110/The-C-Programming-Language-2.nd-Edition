fba1c553b6d8a31dad1c7283a47de863e98bb5b4
m3ndax <adrian.goessl@outlook.com>
2022-06-25 11:57:00 +0200

    update code and comment

--- convert.c
+++ convert.c
@@ -1,21 +1,22 @@
-#include <ctype.h>
+/*include the libraries*/
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
+/*main function*/
 int main(int argc, char *argv[])
 {
-	int		c;
-	
-	while ((c = getchar()) != EOF)
-	
+	int	c;
+
+	while((c = getchar()) != EOF)
+
 		/*Depending of the System, argv[0] may contain the full
-		 *calling path of the program. The simplest solution is to 
+		 *calling path ot the program. The simplest solution is to 
 		 *use strstr() to search for the name.
 		 */
-		 
-		putchar(strstr(argv[0], "lower") != NULL
-			? tolower(c)
+		 putchar(strstr(argv[0], "lower") != NULL
+		 	? tolower(c)
 			: toupper(c));
-			
+
 	return 0;
 }
